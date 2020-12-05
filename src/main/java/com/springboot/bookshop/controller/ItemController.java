e1002b4eecf78f2848872df3acd59a2de90ed4da
Hakim Hani <hakeem@my.yorku.ca>
2020-12-05 12:53:32 -0500

    working showusers and item details

--- src/main/java/com/springboot/bookshop/controller/ItemController.java
+++ src/main/java/com/springboot/bookshop/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.springboot.bookshop.controller;
 
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,20 +16,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.bookshop.entity.ItemInfo;
+import com.springboot.bookshop.entity.User;
 import com.springboot.bookshop.model.Visitor;
+import com.springboot.bookshop.service.AddressService;
 import com.springboot.bookshop.service.ItemInfoService;
+import com.springboot.bookshop.service.UserService;
+import com.springboot.bookshop.utils.ResponseBuilder;
 
 
 
 
 
 @Controller
-@RequestMapping("/item")
+@RequestMapping("/admin_ops")
 @Scope("session")
 public class ItemController {
 
 	@Autowired
 	private ItemInfoService itemInfoService;
+	
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private AddressService addressService;
+
+	@Autowired
+	private Visitor visitor;
+
+	@Autowired
+	private ResponseBuilder responseBuilder;
 
 
 	@GetMapping("/{id}")
@@ -42,6 +60,42 @@ public class ItemController {
 		
 		return ("show_Item");
 	}
+	
+	
+	// get all users
+	@GetMapping("/users")
+	public String getAllUsers(Model model) {
+		List<User> parameters = this.userService.findAll();
+//		List<> parameters = new HashMap<String, List<String>>();
+//		System.out.println("users number" + users.size());
+//		for(User user: users) {
+//			parameters.put("Id", user.getId()+ "");
+//			parameters.put("First Name", user.getFirstName());
+//			parameters.put("Lat Name", user.getLastName());
+//			parameters.put("Email", user.getEmail());
+//			parameters.put("Phone Number", user.getPhone());
+//		}
+		model.addAttribute("parameters",parameters);
+		
+		return ("showUsers");
+	}
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
